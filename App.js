a5713e1bffb54855d04508e3f4211350f48af0bb
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2023-12-10 16:05:03 +0300

    init: start project

---
+++ App.js
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { store } from './redux/store'
+
+import MainPage from './components/MobileCompany';
+
+ReactDOM.render(
+  <Provider store={store}>
+      <MainPage/>
+  </Provider>, document.getElementById('container')
+  );
+
